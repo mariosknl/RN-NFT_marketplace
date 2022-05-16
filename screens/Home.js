74aaa84a302236ce31e39830a0dbd318cdc6d7dd
mariosknl <mariosknl@outlook.com>
2022-05-16 09:29:35 +0300

    editing the layout for nftcard component

--- screens/Home.js
+++ screens/Home.js
@@ -7,13 +7,13 @@ import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 const Home = () => {
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
-			<FocusedStatusBar background={COLORS.primary} />
+			<FocusedStatusBar backgroundColor={COLORS.primary} />
 
 			<View style={{ flex: 1 }}>
 				<View style={{ zIndex: 0 }}>
 					<FlatList
 						data={NFTData}
-						renderItem={({ item }) => <Text>{item.name}</Text>}
+						renderItem={({ item }) => <NFTCard data={item} />}
 						keyExtractor={(item) => item.id}
 						showsVerticalScrollIndicator={false}
 						ListHeaderComponent={<HomeHeader />}
